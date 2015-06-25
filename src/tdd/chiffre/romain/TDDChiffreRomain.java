3e41689b32ddc9a0b1365004c3445ecf383c12dd
Lucas <lucas.girardin@outlook.com>
2015-06-25 21:18:52 +0200

    First test fail

---
+++ src/tdd/chiffre/romain/TDDChiffreRomain.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tdd.chiffre.romain;
+
+/**
+ *
+ * @author Lucas
+ */
+public class TDDChiffreRomain {
+
+    /**
+     * @param args the command line arguments
+     */
+    /* Les chiffre romain à tester :
+    
+    
+    */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }
+    
+    public static String ConvertRomanNumber(int number){
+        
+        return null;
+    }
+}
---
+++ test/TestConvertRomanNumber.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import tdd.chiffre.romain.TDDChiffreRomain;
+
+
+/**
+ *
+ * @author Lucas
+ */
+public class TestConvertRomanNumber {
+    
+    public TestConvertRomanNumber() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    
+    @Test
+    public void number1() {
+    
+        String expect = "I";
+        String result = TDDChiffreRomain.ConvertRomanNumber(1);
+        assertEquals(expect, result);
+        
+    }
+}
